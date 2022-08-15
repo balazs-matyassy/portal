edbc39bb2fd4e52a599d86f87ced1a53b49f6042
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-08-15 17:32:42 +0200

    Initial commit

---
+++ src/test/java/hu/progmatic/portal/PortalApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.progmatic.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
